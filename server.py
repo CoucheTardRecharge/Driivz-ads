0da56a267a658df16de33fdc52c152f1d0c37612
Circle K <emile.trottier@couche-tard.com>
2026-09-16 16:57:53 -0400

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from urllib.parse import urlparse, parse_qs, unquote
 # ==========================================================
 
 ADS_BY_PROPERTY = {
-    "45": "Car-wash.png"
+    "1148461": "Car-wash.png"
 }
 
 
